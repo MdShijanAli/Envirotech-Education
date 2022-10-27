932851c4ce2616b12cac2df13e7750a104e06068
FreelancerShijan <freelancershijan@gmail.com>
2022-10-27 23:21:08 +0600

    Finished

--- src/components/Context/AuthProvider/AuthProvider.js
+++ src/components/Context/AuthProvider/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, updateProfile, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, updateProfile, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, sendPasswordResetEmail } from "firebase/auth";
 import app from '../../../firebase/firebase.config';
 
 export const AuthContext = createContext();
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,10 +9,10 @@ const Profile = () => {
 
         <div className="bg-purple-400 shadow-xl my-12 pb-6 mx-5 justify-center items-center overflow-hidden md:w-1/2 rounded-lg md:mx-auto">
             <div className="relative h-40">
-                <img className="absolute h-full w-full object-cover" src="https://images.unsplash.com/photo-1448932133140-b4045783ed9e?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80" />
+                <img className="absolute h-full w-full object-cover" alt='BannerPhoto' src="https://images.unsplash.com/photo-1448932133140-b4045783ed9e?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80" />
             </div>
             <div className="relative shadow mx-auto h-36 w-36 -mt-20 border-white rounded-full overflow-hidden border-4">
-                <img className="object-cover w-full h-full" src={user?.photoURL} />
+                <img className="object-cover w-full h-full" alt='profilePhoto' src={user?.photoURL} />
             </div>
             <div className="mt-10">
                 <h1 className="text-2xl text-center font-semibold">
